d5c77473a6ccd1645bd1b9066b309c6523413eb5
Manas Mishra <manas@7halo.in>
2017-05-08 21:13:17 +0530

    Dashboard, pull refresh

---
+++ SuperBSwift/DashboardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardTableViewCell.swift
+//  SuperBSwift
+//
+//  Created by Nauroo on 08/05/17.
+//  Copyright © 2017 Manas. All rights reserved.
+//
+
+import UIKit
+
+class DashboardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
